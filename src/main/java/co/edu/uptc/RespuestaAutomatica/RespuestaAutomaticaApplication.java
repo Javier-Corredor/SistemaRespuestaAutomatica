58d0cdcb8653e32b31fa2457f2861d9f595a2570
Javier Az <javiaz466@gmail.com>
2025-11-10 10:40:18 -0500

    Commit inicial: Proyecto de Spring Boot cargado

---
+++ src/main/java/co/edu/uptc/RespuestaAutomatica/RespuestaAutomaticaApplication.java
@@ -0,0 +1,13 @@
+package co.edu.uptc.RespuestaAutomatica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RespuestaAutomaticaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RespuestaAutomaticaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/edu/uptc/RespuestaAutomatica/RespuestaAutomaticaApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.uptc.RespuestaAutomatica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RespuestaAutomaticaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
